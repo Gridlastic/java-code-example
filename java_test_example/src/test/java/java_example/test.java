aef9ab455ad7dca99917ae70be0e8f9acddc7648
admin <admin@gridlastic.com>
2015-10-14 12:52:03 -0700

    Updates for latest selenium version and Linux testing.

--- java_test_example/src/test/java/java_example/test.java
+++ java_test_example/src/test/java/java_example/test.java
@@ -1,10 +1,17 @@
+// NOTE: replace USERNAME:ACCESS_KEY@SUBDOMAIN and VIDEO_URL with your credentials found in the Gridlastic dashboard
+// ALSO SEE https://github.com/Gridlastic/demo1 FOR JAVA TESTNG EXAMPLES WITH PARALLEL TEST EXECUTIONS
+// NOTE: THE FIRST TEST ON A GRID NODE AFTER BEING LAUNCHED CAN BE SLOW, FOLLOWING TESTS ARE MUCH FASTER AFTER THE NODE IS "WARMED" UP.
+
 package java_example;
 
 import java.net.URL;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -14,7 +21,9 @@ import org.testng.annotations.Test;
 
 public class test {
 
-	private static final String VIDEO_URL = null;
+	// VIDEO_URL set to like "https://s3-ap-southeast-2.amazonaws.com/b2729248-ak68-6948-a2y8-80e7479te16a/9ag7b09j-6a38-58w2-bb01-17qw724ce46t/play.html?".
+	// Find this VIDEO_URL value in your Gridlastic dashboard.
+	private static final String VIDEO_URL = null; 
 	private RemoteWebDriver driver;
 
 	@BeforeMethod(alwaysRun = true)
@@ -22,9 +31,9 @@ public class test {
 
 		// Example test environment. NOTE: Gridlastic auto scaling requires all
 		// these 3 environment variables in each request.
-		String platform_name = "linux";
+		String platform_name = "win7";
 		String browser_name = "firefox";
-		String browser_version = "36"; // for Chrome leave empty
+		String browser_version = "41"; // for Chrome leave empty
 
 		// optional video recording
 		String record_video = "True";
@@ -54,18 +63,24 @@ public class test {
 		
 		if (browser_name.equalsIgnoreCase("chrome")){
 			ChromeOptions options = new ChromeOptions();
-			options.addArguments(Arrays.asList("--start-maximized"));
+			// On Linux start-maximized does not expand browser window to max screen size. Always set a window size.
+			if (platform_name.equalsIgnoreCase("linux")) {
+				options.addArguments(Arrays.asList("--window-size=1920,1080"));	
+				} else {
+				options.addArguments(Arrays.asList("--start-maximized"));
+				}
 			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 			} 
 		
 	
-		
-		driver = new RemoteWebDriver(
-				new URL(
-						"http://USERNAME:ACCESS_KEY@SUBDOMAIN.gridlastic.com:4444/wd/hub"),
-				capabilities);
+		//replace USERNAME:ACCESS_KEY@SUBDOMAIN with your credentials found in the Gridlastic dashboard
+		driver = new RemoteWebDriver(new URL("http://USERNAME:ACCESS_KEY@SUBDOMAIN.gridlastic.com:80/wd/hub"),capabilities);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+		
+        // On LINUX/FIREFOX the "driver.manage().window().maximize()" option does not expand browser window to max screen size. Always set a window size.
+    	if (platform_name.equalsIgnoreCase("linux") && browser_name.equalsIgnoreCase("firefox")) {
+    		driver.manage().window().setSize(new Dimension(1920, 1080));	
+    	}
         
 
 		if (record_video.equalsIgnoreCase("True")) {
@@ -75,11 +90,13 @@ public class test {
 	}
 
 	@Test(enabled = true)
-	public void test_google() {
-		driver.get("http://www.java.com");
-		driver.findElementByLinkText("About Java").click();
-		driver.findElementByLinkText("Troubleshoot Java").click();
-		driver.findElementByLinkText("Support");
+	 public void test_site() throws Exception  { 	
+		driver.get("https://www.google.com/ncr");
+		Thread.sleep(10000); //slow down for demo purposes
+		WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("webdriver");
+        element.submit();
+        Thread.sleep(5000);
 	}
 
 	@AfterMethod(alwaysRun = true)
